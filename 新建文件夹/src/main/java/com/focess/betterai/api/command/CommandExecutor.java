403fd2d37d8ee0248341203aa56117b1eb824971
Object_ <478322272@qq.com>
2020-08-18 17:22:18 +0800

    去除冗余

--- 新建文件夹/src/main/java/com/focess/betterai/api/command/CommandExecutor.java
+++
@@ -1,9 +0,0 @@
-package com.focess.betterai.api.command;
-
-import org.bukkit.command.CommandSender;
-
-public interface CommandExecutor {
-
-    void execute(CommandSender sender, String[] args);
-
-}
